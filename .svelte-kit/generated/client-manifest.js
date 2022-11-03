7b43f8cb7ea93623b35f3cc4c2fb9d7904619c4e
mochamello <49714750+mochamellow@users.noreply.github.com>
2022-11-04 00:31:51 +0800

    changesbash: line 1: changes: command not found

--- .svelte-kit/generated/client-manifest.js
+++ .svelte-kit/generated/client-manifest.js
@@ -2,12 +2,16 @@ export { matchers } from './client-matchers.js';
 
 			export const nodes = [() => import('./nodes/0'),
 	() => import('./nodes/1'),
-	() => import('./nodes/2')];
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4')];
 
 			export const server_loads = [];
 
 			export const dictionary = {
-	"/": [2]
+	"/": [2],
+	"/how-intentio-works": [3],
+	"/intent-questions": [4]
 };
 
 			export const hooks = {
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import preprocessor from "svelte-preprocess";
+
+const dev = "production" === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocessor(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: "docs",
+			assets: "docs", 
+			fallback: "index.html"
+		}),
+		// paths: {
+		// 	base: dev ? "" : "/intentio-gh",
+		// },
 	}
 };
 
